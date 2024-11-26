5a2f9515ad1b254da2f41cb0c4a15c6b2878150d
mohammed <mohammed@veeqo.com>
2024-11-26 19:12:06 +0000

    Auto commit

--- config/puma.rb
+++ config/puma.rb
@@ -1,36 +1,40 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+# Specifies the number of Puma workers to run. Each worker is a forked OS process.
+# Workers are usually set to match the number of available CPU cores.
+workers Integer(ENV.fetch('WEB_CONCURRENCY', 2))
 
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-workers Integer(ENV['WEB_CONCURRENCY'] || 2) # Adjust to a lower number
-min_threads_count = Integer(ENV['RAILS_MIN_THREADS'] || 1)
-max_threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+# Threads are specified per worker.
+min_threads_count = Integer(ENV.fetch('RAILS_MIN_THREADS', 5))
+max_threads_count = Integer(ENV.fetch('RAILS_MAX_THREADS', 5))
 threads min_threads_count, max_threads_count
 
-# Specifies that the worker count should equal the number of processors in production.
-if ENV["RAILS_ENV"] == "production"
-  require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
-  workers worker_count if worker_count > 1
-end
-
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
-
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+port ENV.fetch('PORT', 3000)
 
-# Specifies the `environment` that Puma will run in.
-environment ENV.fetch("RAILS_ENV") { "development" }
+# Specifies the environment Puma will run in.
+environment ENV.fetch('RAILS_ENV', 'production')
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
+
+# Specifies the location of Puma's state file.
+state_path ENV.fetch('STATEFILE', 'tmp/pids/puma.state')
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+# Enable preload of the application for faster worker boot times.
+preload_app!
+
+# Configure worker timeout in development to allow longer debugging sessions.
+worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
+
+# Configure hooks to manage connections and clean up before forking.
+on_worker_boot do
+  # Reconnect to database
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
+
+# Gracefully shut down workers on restart.
+on_restart do
+  puts 'Performing restart cleanup...'
+end
