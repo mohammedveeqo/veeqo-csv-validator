0ee706f1b0f8863b79ead80c89c3f09632e91a88
mohammed <mohammed@veeqo.com>
2024-11-28 09:58:14 +0000

    fix

--- app/models/csv_upload.rb
+++ app/models/csv_upload.rb
@@ -1,5 +1,5 @@
 class CsvUpload < ApplicationRecord
   # Add any validations or relationships here if needed
   validates :file_name, presence: true
-  validates :file_type, presence: true
+  validates :upload_type, presence: true  # Change this to :upload_type
 end
--- db/migrate/20241120122221_create_csv_uploads.rb
+++ db/migrate/20241120122221_create_csv_uploads.rb
@@ -1,10 +1,10 @@
 class CreateCsvUploads < ActiveRecord::Migration[7.1]
-  def change
-    create_table :csv_uploads do |t|
-      t.string :file
-      t.string :type
-      t.timestamps
-    end
+  create_table :csv_uploads do |t|
+    t.string :file_name
+    t.string :upload_type
+    t.text :validation_errors
+    t.timestamps
+  end
   end
 end
 
