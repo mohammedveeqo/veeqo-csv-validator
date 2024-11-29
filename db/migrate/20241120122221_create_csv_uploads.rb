d6be6837c156a073c274dbd283f2bf4ee228ddb9
mohammed <mohammed@veeqo.com>
2024-11-29 13:10:19 +0000

    fix

--- db/migrate/20241120122221_create_csv_uploads.rb
+++ db/migrate/20241120122221_create_csv_uploads.rb
@@ -1,10 +1,11 @@
 class CreateCsvUploads < ActiveRecord::Migration[7.1]
-  create_table :csv_uploads do |t|
-    t.string :file_name
-    t.string :upload_type
-    t.text :validation_errors
-    t.timestamps
-  end
+  def change
+    create_table :csv_uploads do |t|
+      t.string :file_name
+      t.string :upload_type
+      t.text :validation_errors
+
+      t.timestamps
+    end
   end
 end
-
